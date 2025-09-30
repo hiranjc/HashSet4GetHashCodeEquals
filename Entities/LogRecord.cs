da7c8e021aa400421a1fc876010a0bd7958123b7
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-30 18:40:48 -0300

    first commit

---
+++ Entities/LogRecord.cs
@@ -0,0 +1,23 @@
+﻿namespace HashSet4GetHashCodeEquals.Entities
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
